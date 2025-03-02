c61edd282681199cb48db694d19c30d3d7f6290a
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-03-01 23:13:27 -0600

    Added main.py with functionality to read files and output their text onto stdout

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+# BookBot: My first guided project from boot.dev
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read() # This method actually reads the file and saves its contents as a string
+    return file_contents
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+main()
