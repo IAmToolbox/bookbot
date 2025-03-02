12a2c91cc67f98b947c799e9bb2556c0f9efd908
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-03-01 23:24:26 -0600

    Changed main.py functionality. It counts words in a document now

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ def get_book_text(filepath):
         file_contents = f.read() # This method actually reads the file and saves its contents as a string
     return file_contents
 
+def count_words(string):
+    words = string.split()
+    word_count = 0
+    for word in words: # Loops once for every word in the input string
+        word_count += 1
+    return word_count
+
+
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    word_count = count_words(get_book_text("./books/frankenstein.txt"))
+    print(f"{word_count} words found in the document") # I love f-strings
 
 main()
