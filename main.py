203edcd3c11b7ea9f8355bebc3c8d13cab538c7f
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-03-02 21:18:33 -0600

    Added list sorting and printing of a pretty report

--- main.py
+++ main.py
@@ -2,16 +2,31 @@
 
 from stats import count_words
 from stats import count_characters
+from stats import sort_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read() # This method actually reads the file and saves its contents as a string
     return file_contents
 
+def print_report(word_count, sorted_characters):
+    print("============ BOOKBOT ============")
+    print("Analyzing book fount at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for i in range(len(sorted_characters)):
+        if sorted_characters[i]["character"].isalpha():
+            print(f"{sorted_characters[i]["character"]}: {sorted_characters[i]["count"]}")
+    print("============= END ===============")
+
 def main():
     word_count = count_words(get_book_text("./books/frankenstein.txt"))
-    print(f"{word_count} words found in the document") # I love f-strings
+    #print(f"{word_count} words found in the document") # I love f-strings
     character_count = count_characters(get_book_text("./books/frankenstein.txt"))
-    print(character_count)
+    sorted_characters = sort_characters(character_count)
+    #print(character_count)
+    #print(sorted_characters)
+    print_report(word_count, sorted_characters)
 
 main()
--- stats.py
+++ stats.py
@@ -16,3 +16,16 @@ def count_characters(string):
         else:
             character_dict[character] = 1
     return character_dict
+
+# Time to figure out how to sort this bullshit
+
+def sort_on(dict): # Function to get a proper sorted list
+    return dict["count"]
+
+def sort_characters(character_dict):
+    sorted_list = [] # Initialize empty list for sorting
+    for character in character_dict:
+        sorted_list.append({"character": character, "count": character_dict[character]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+    
