bc36db29c17bd996641496ce21d304ff76b16cb9
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-03-02 21:27:58 -0600

    Added functionality for analyzing other books via stdin arguments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # BookBot: My first guided project from boot.dev
 
+import sys
+
 from stats import count_words
 from stats import count_characters
 from stats import sort_characters
@@ -11,7 +13,7 @@ def get_book_text(filepath):
 
 def print_report(word_count, sorted_characters):
     print("============ BOOKBOT ============")
-    print("Analyzing book fount at books/frankenstein.txt...")
+    print(f"Analyzing book fount at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
@@ -21,9 +23,12 @@ def print_report(word_count, sorted_characters):
     print("============= END ===============")
 
 def main():
-    word_count = count_words(get_book_text("./books/frankenstein.txt"))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    word_count = count_words(get_book_text(sys.argv[1]))
     #print(f"{word_count} words found in the document") # I love f-strings
-    character_count = count_characters(get_book_text("./books/frankenstein.txt"))
+    character_count = count_characters(get_book_text(sys.argv[1]))
     sorted_characters = sort_characters(character_count)
     #print(character_count)
     #print(sorted_characters)
